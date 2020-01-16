ea692483b573e02c394db6eec37eca6828c733e8
Rakshak D S <wasdlifeqecrlove@gmail.com>
2020-01-16 16:24:56 +0530

    [rak] Binary Search of a String pushed

---
+++ src/BinarySearch.kt
@@ -0,0 +1,22 @@
+fun main(args: Array<String>) {
+    val arr = arrayOf("rak", "rama", "naanu", "c#")
+    val x = "rak"
+    val z = BinarySearchOfAString()
+    val result: Int = z.binarySearch(arr, x)
+    if (result == -1) println("String is not found") else println("String found at index ===>>> $result")
+
+}
+
+class BinarySearchOfAString {
+    fun binarySearch(arr: Array<String>, x: String): Int {
+        var l = 0
+        var r = arr.size - 1
+        while (l <= r) {
+            val m = l + (r - l) / 2
+            val res = x.compareTo(arr[m]!!)
+            if (res == 0) return m
+            if (res > 0) l = m + 1 else r = m - 1
+        }
+        return -1
+    }
+}
