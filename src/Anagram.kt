c7846613eea052b27315b45327e73076e10232c4
Rakshak D S <wasdlifeqecrlove@gmail.com>
2020-01-16 17:51:27 +0530

    [rak] Anagram detection program pushed

--- src/Anagram.kt
+++ src/Anagram.kt
@@ -6,3 +6,21 @@
  *  @since:16-01-2020
  *
  ******************************************************************************/
+import java.util.*
+
+fun main() {
+    println("Enter String1 for comparison")
+    var str1: String? = readLine()
+    println("Enter String2 for comparison")
+    var str2: String? = readLine()
+
+    var isAnagram = Arrays.equals(
+        str1?.chars()?.sorted()?.toArray(),
+        str2?.chars()?.sorted()?.toArray()
+    );
+
+    if (isAnagram)
+        println("Strings are an anagrams of each other");
+    else
+        println("Strings are not Anagrams of each other");
+}
